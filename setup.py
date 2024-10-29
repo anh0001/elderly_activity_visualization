a17d594827bb2bd49c07b4216562fac55326ba4e
Anhar Risnumawan <anhrisn@gmail.com>
2024-10-30 08:12:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-         glob(os.path.join('launch', '*.launch.py'))),
+        # Add config files
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # Add launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -30,3 +32,4 @@ setup(
         ],
     },
 )
+```
