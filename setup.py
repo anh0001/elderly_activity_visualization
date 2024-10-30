7512bab4af82a701e044ff86b9607127f3cca99d
anh0001 <anhrisn@gmail.com>
2024-10-30 14:00:24 +0900

    Update

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ package_name = 'elderly_activity_visualization'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', 
             ['resource/' + package_name]),
