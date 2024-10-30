75ae562387f82f089495be46aafadabf60468e84
anh0001 <anhrisn@gmail.com>
2024-10-30 11:15:27 +0900

    Update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-import os
 from glob import glob
+import os
 
 package_name = 'elderly_activity_visualization'
 
@@ -9,20 +9,21 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
+        # This line adds the marker file
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Add config files
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        # Add launch files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), 
+            glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), 
+            glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Anhar Risnumawan',
-    maintainer_email='anhrisn@gmail.com',
-    description='Elderly activity visualization using radar charts',
-    license='Apache License 2.0',
+    maintainer='mobi',
+    maintainer_email='mobi@todo.todo',
+    description='Elderly activity visualization for NCGG',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
